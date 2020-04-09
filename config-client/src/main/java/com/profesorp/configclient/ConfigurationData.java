82e8a2e4414f75defce647c26b29f0a4b3d85988
dujfalusi <dujfalusi@apptium.com>
2020-04-09 13:18:06 +0200

    fixed property naming

--- config-client/src/main/java/com/profesorp/configclient/ConfigurationData.java
+++ config-client/src/main/java/com/profesorp/configclient/ConfigurationData.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 @Data
 @Component
-@ConfigurationProperties("serverData")
+@ConfigurationProperties("serverdata")
 public class ConfigurationData {	
 	private int minimum;
 	private int maximum;
