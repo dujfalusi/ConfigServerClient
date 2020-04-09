038a30cade459181a0fe7e0520d2519acf1be6a1
dujfalusi <dujfalusi@apptium.com>
2020-04-09 13:58:58 +0200

    fixed property naming

--- config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
+++ config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
@@ -3,9 +3,11 @@ package com.profesorp.configclient;
 import com.profesorp.configclient.bean.BeanConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class ConfigServiceController {
 
@@ -28,12 +30,12 @@ public class ConfigServiceController {
   }
 
   @GetMapping("/refreshed")
-  public BeanConfiguration getConfigurationRefreshed(@Value("${values.function_value}") String functionValue) {
+  public BeanConfiguration getConfigurationRefreshed(@Value("${values.function_value}") String functionValueUpdated) {
     return new BeanConfiguration(configuration.getMinimum(),
-            configuration.getMaximum(), permanentValue, functionValue);
+            configuration.getMaximum(), permanentValue, functionValueUpdated);
   }
 
-  @GetMapping("/data")
+  @GetMapping("/serverData")
   public ConfigurationData retrieveDataFromConfiguration() {
     return configurationData;
   }
